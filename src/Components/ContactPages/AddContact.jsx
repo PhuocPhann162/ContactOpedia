983e126f80cf3004bd2ce41ac74b717fc2134b5a
Phuoc Phan <21520405@gm.uit.edu.vn>
2023-11-09 13:21:01 +0700

    done ui

--- src/Components/ContactPages/AddContact.jsx
+++ src/Components/ContactPages/AddContact.jsx
@@ -1,18 +1,31 @@
 const AddContact = () => {
   return (
-    <div className="border row text-white p-2">
-      <div className="col-12 text-white-50">Add a new Contact</div>
-      <div className="col-12 col-md-4 p-1">
-        <input className="form-control form-control-sm" placeholder="Name..." />
-      </div>
-      <div className="col-12 col-md-4 p-1">
-        <input className="form-control form-control-sm" placeholder="Email..." />
-      </div>
-      <div className="col-12 col-md-4 p-1">
-        <input className="form-control form-control-sm" placeholder="Phone..." />
-      </div>
-      <div className="col-12 col-md-6 offset-md-3 p-1">
-        <button className="btn btn-primary btn-sm form-control">Create</button>
+    <div className="border col-12 text-white p-2">
+      <div className="row p-2">
+        <div className="col-12 text-white-50">Add a new Contact</div>
+        <div className="col-12 col-md-4 p-1">
+          <input
+            className="form-control form-control-sm"
+            placeholder="Name..."
+          />
+        </div>
+        <div className="col-12 col-md-4 p-1">
+          <input
+            className="form-control form-control-sm"
+            placeholder="Email..."
+          />
+        </div>
+        <div className="col-12 col-md-4 p-1">
+          <input
+            className="form-control form-control-sm"
+            placeholder="Phone..."
+          />
+        </div>
+        <div className="col-12 col-md-6 offset-md-3 p-1">
+          <button className="btn btn-primary btn-sm form-control">
+            Create
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/Components/ContactPages/Contact.jsx
+++ src/Components/ContactPages/Contact.jsx
@@ -1,10 +1,48 @@
-
 const Contact = (props) => {
-    return (
-        <div>
-            <button className="btn btn-secondary form-control">{props.contact.name}</button>
+  return (
+    <div
+      className="row p-md-2 mb-2"
+      style={{ border: "1px solid #555", borderRadius: "20px" }}
+    >
+      <div className="col-2 col-md-1 pt-2 pt-md-1">
+        <img
+          src={`https://ui-avatars.com/api/?name=${props.contact.name}`}
+          alt="KW"
+          style={{ width: "80%" }}
+        />
+      </div>
+      <div className="col-6 col-md-5 text-warning pt-0">
+        <span className="h4">{props.contact.name}</span>
+        <br />
+        <div className="text-white-50">
+          <span>{props.contact.email}</span>
+          <br />
+          <span>{props.contact.phone}</span>
         </div>
-    );
-}
+      </div>
+      <div className="col-2 col-md-2 pt-md-3">
+        <button
+          className={`btn btn-sm m-1 ${
+            props.contact.isFavorite ? "btn-warning" : "btn-outline-warning"
+          }`}
+        >
+          <i class="bi bi-star" style={{fontSize: "1rem"}}></i>
+        </button>
+      </div>
+      <div className="col-2 col-md-3 pt-md-3">
+        <button
+          className="btn btn-primary btn-sm m-1" 
+        >
+          <i class="bi bi-pencil-square" style={{fontSize: "1rem"}}></i>
+        </button>
+        <button
+          className="btn btn-danger btn-sm m-1" 
+        >
+          <i class="bi bi-trash3" style={{fontSize: "1rem"}}></i>
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
